79803aa73303b8d2b9b614428b0afe2bf77ab0c6
Nathan Taylor <ntaylor@cs.utexas.edu>
2023-07-03 13:40:38 -0500

    add tests

---
+++ src/tests/passes/isolate_normalizer.rs
@@ -0,0 +1,201 @@
+#[cfg(test)]
+mod tests {
+    use crate::ast::declarations::{Decl, NormalizedIsolateDecl};
+    use crate::parser::ivy::{IvyParser, Rule};
+    use crate::passes::isolate_normalizer::{IsolateNormalizer, NormalizerError};
+    use crate::visitor::visitor::Visitable;
+    use pest_consume::Parser;
+
+    fn decl_from_src(prog: &str) -> Decl {
+        let res = IvyParser::parse(Rule::decl, &prog)
+            .expect("Parsing failed")
+            .single()
+            .unwrap();
+        IvyParser::decl(res).expect("AST generation failed")
+    }
+
+    #[test]
+    fn normalize_empty_module() {
+        let mut ast = decl_from_src("isolate foo = {}");
+        let mut mn = IsolateNormalizer::new();
+
+        let expected_ast = Decl::NormalizedIsolate(NormalizedIsolateDecl {
+            name: "foo".into(),
+            params: vec![],
+            impl_decls: vec![],
+            spec_decls: vec![],
+            common_spec_decls: vec![],
+            common_impl_decls: vec![],
+        });
+        ast.visit(&mut mn).expect("traversal");
+        assert_eq!(ast, expected_ast);
+    }
+
+    #[test]
+    fn normalize_simple_module() {
+        let mut ast = decl_from_src(
+            "isolate foo = {
+            var is_up: bool;
+            instance net: sock.net;
+        }",
+        );
+        let expected_ast = Decl::NormalizedIsolate(NormalizedIsolateDecl {
+            name: "foo".into(),
+            params: vec![],
+            impl_decls: [
+                decl_from_src("var is_up: bool"),
+                decl_from_src("instance net: sock.net"),
+            ]
+            .into(),
+            spec_decls: vec![],
+            common_spec_decls: vec![],
+            common_impl_decls: vec![],
+        });
+
+        let mut mn = IsolateNormalizer::new();
+        ast.visit(&mut mn).expect("traversal");
+        assert_eq!(ast, expected_ast);
+    }
+
+    #[test]
+    fn normalize_impl_module() {
+        let mut ast = decl_from_src(
+            "isolate foo = {
+            instance net: sock.net;
+
+            implementation {
+                var is_up: bool;
+            }
+        }",
+        );
+        let expected_ast = Decl::NormalizedIsolate(NormalizedIsolateDecl {
+            name: "foo".into(),
+            params: vec![],
+            impl_decls: [
+                decl_from_src("instance net: sock.net"),
+                decl_from_src("var is_up: bool"),
+            ]
+            .into(),
+            spec_decls: vec![],
+            common_spec_decls: vec![],
+            common_impl_decls: vec![],
+        });
+
+        let mut mn = IsolateNormalizer::new();
+        ast.visit(&mut mn).expect("traversal");
+        assert_eq!(ast, expected_ast);
+    }
+
+    #[test]
+    fn normalize_multiple_impls() {
+        let mut ast = decl_from_src(
+            "isolate foo = {
+            implementation {
+                instance net: sock.net;
+            }
+
+            implementation {
+                var is_up: bool;
+            }
+        }",
+        );
+        let expected_ast = Decl::NormalizedIsolate(NormalizedIsolateDecl {
+            name: "foo".into(),
+            params: vec![],
+            impl_decls: [
+                decl_from_src("instance net: sock.net"),
+                decl_from_src("var is_up: bool"),
+            ]
+            .into(),
+            spec_decls: vec![],
+            common_spec_decls: vec![],
+            common_impl_decls: vec![],
+        });
+
+        let mut mn = IsolateNormalizer::new();
+        ast.visit(&mut mn).expect("traversal");
+        assert_eq!(ast, expected_ast);
+    }
+
+    #[test]
+    fn normalize_nested_decls() {
+        let mut ast = decl_from_src(
+            "isolate foo = {
+            implementation {
+                implementation {
+                    instance net: sock.net;
+                }
+            }
+
+            implementation {
+                var is_up: bool;
+            }
+        }",
+        );
+
+        let mut mn = IsolateNormalizer::new();
+        let res = ast.visit(&mut mn).expect_err("traversal");
+        assert_eq!(
+            res.downcast::<NormalizerError>().unwrap(),
+            NormalizerError::BadNesting {
+                inner: "implementation",
+                outer: "implementation"
+            }
+        );
+    }
+
+    #[test]
+    fn normalize_simple_common() {
+        let mut ast = decl_from_src(
+            "isolate foo = {
+            common {
+                var is_up: bool;
+                instance net: sock.net;
+            }
+        }",
+        );
+        let expected_ast = Decl::NormalizedIsolate(NormalizedIsolateDecl {
+            name: "foo".into(),
+            params: vec![],
+            impl_decls: vec![],
+            spec_decls: vec![],
+            common_spec_decls: vec![],
+            common_impl_decls: [
+                decl_from_src("var is_up: bool"),
+                decl_from_src("instance net: sock.net"),
+            ]
+            .into(),
+        });
+        let mut mn = IsolateNormalizer::new();
+        ast.visit(&mut mn).expect("traversal");
+        assert_eq!(ast, expected_ast);
+    }
+
+    #[test]
+    fn normalize_common() {
+        let mut ast = decl_from_src(
+            "isolate foo = {
+            common {
+                specification {
+                    var is_up: bool;
+                }
+                implementation {
+                    instance net: sock.net;
+                }
+            }
+        }",
+        );
+        let expected_ast = Decl::NormalizedIsolate(NormalizedIsolateDecl {
+            name: "foo".into(),
+            params: vec![],
+            impl_decls: vec![],
+            spec_decls: vec![],
+            common_spec_decls: [decl_from_src("var is_up: bool")].into(),
+            common_impl_decls: [decl_from_src("instance net: sock.net")].into(),
+        });
+
+        let mut mn = IsolateNormalizer::new();
+        ast.visit(&mut mn).expect("traversal");
+        assert_eq!(ast, expected_ast);
+    }
+}
---
+++ src/tests/typechecker/processes.rs
@@ -0,0 +1,143 @@
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{
+        ast::{declarations::Decl, expressions::Expr},
+        parser::ivy::{IvyParser, Rule},
+        passes::isolate_normalizer::IsolateNormalizer,
+        typechecker::{
+            inference::TypeChecker,
+            sorts::{IvySort, Process},
+        },
+        visitor::visitor::Visitable,
+    };
+    use pest_consume::Parser;
+
+    fn isolate_from_src(prog: &str) -> Decl {
+        let res = IvyParser::parse(Rule::isolate_decl, &prog)
+            .expect("Parsing failed")
+            .single()
+            .unwrap();
+        let mut proc = Decl::Isolate(IvyParser::isolate_decl(res).expect("AST generation failed"));
+
+        // Since we'll be assuming that the normalizer has run prior to
+        // typechecking, enforce it here.
+        let mut norm = IsolateNormalizer::new();
+        proc.visit(&mut norm).unwrap().modifying(&mut proc).unwrap();
+        proc
+    }
+
+    fn typechecker_with_bindings() -> TypeChecker {
+        let mut tc = TypeChecker::new();
+
+        // type pid: 0..3
+        tc.bindings
+            .append(
+                "pid".into(),
+                IvySort::Range(Box::new(Expr::Number(0)), Box::new(Expr::Number(3))),
+            )
+            .unwrap();
+        // interpret byte -> bv[8]
+        tc.bindings
+            .append("byte".into(), IvySort::BitVec(8))
+            .unwrap();
+
+        tc
+    }
+
+    #[test]
+    fn test_empty_process() {
+        let mut iso = isolate_from_src("process p = { }");
+
+        let sort = IvySort::Process(Process {
+            args: vec![],
+            impl_fields: HashMap::new(),
+            spec_fields: HashMap::new(),
+            common_impl_fields: HashMap::new(),
+            common_spec_fields: HashMap::new(),
+        });
+
+        let mut tc = TypeChecker::new();
+        let res = iso.visit(&mut tc).unwrap().modifying(&mut iso).unwrap();
+        assert_eq!(res, sort);
+
+        assert_eq!(tc.bindings.lookup(&"p".to_owned()), Some(sort));
+    }
+
+    #[test]
+    fn test_proc_with_params() {
+        let mut iso = isolate_from_src("process host(self:pid) = {}");
+        let sort = IvySort::Process(Process {
+            args: vec![(
+                "self".into(),
+                IvySort::Range(Box::new(Expr::Number(0)), Box::new(Expr::Number(3))),
+            )],
+            impl_fields: HashMap::new(),
+            spec_fields: HashMap::new(),
+            common_impl_fields: HashMap::new(),
+            common_spec_fields: HashMap::new(),
+        });
+
+        let mut tc = typechecker_with_bindings();
+        let res = iso.visit(&mut tc).unwrap().modifying(&mut iso).unwrap();
+        assert_eq!(res, sort);
+
+        assert_eq!(tc.bindings.lookup(&"host".to_owned()), Some(sort));
+    }
+
+    #[test]
+    fn test_proc_with_implicit_impl() {
+        let mut iso = isolate_from_src(
+            "process host(self:pid) = {
+            var is_up: bool
+        }",
+        );
+        let sort = IvySort::Process(Process {
+            args: vec![(
+                "self".into(),
+                IvySort::Range(Box::new(Expr::Number(0)), Box::new(Expr::Number(3))),
+            )],
+            impl_fields: [("is_up".into(), IvySort::Bool)].into(),
+            spec_fields: HashMap::new(),
+            common_impl_fields: HashMap::new(),
+            common_spec_fields: HashMap::new(),
+        });
+
+        let mut tc = typechecker_with_bindings();
+        let res = iso.visit(&mut tc).unwrap().modifying(&mut iso).unwrap();
+        assert_eq!(res, sort);
+
+        assert_eq!(tc.bindings.lookup(&"host".to_owned()), Some(sort),);
+    }
+
+    //#[test]
+    fn test_append1_host() {
+        let mut iso = isolate_from_src(
+            "process host(self:pid) = {
+            export action append(val: byte)
+            import action show(content: file)
+            instance sock: net.socket
+            var contents: file
+
+            after init {
+                contents := file.empty;
+            }
+
+            implement append {
+                contents := contents.append(val);
+                sock.send(host(1-self).sock.id, val);
+                show(contents);
+            }
+
+            implement sock.recv(src: tcp.endpoint, val:byte) {
+                contents := contents.append(val);
+                show(contents);
+            }
+        }",
+        );
+
+        let mut tc = typechecker_with_bindings();
+        let _res = iso.visit(&mut tc).unwrap().modifying(&mut iso).unwrap();
+    }
+}
